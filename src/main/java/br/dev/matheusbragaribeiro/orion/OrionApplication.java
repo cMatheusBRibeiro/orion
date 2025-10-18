16f40b13dbf297e75a558bccb7a104f071879663
Matheus Braga Ribeiro <mbribeiropr@gmail.com>
2025-10-18 17:14:56 -0300

    Init project

---
+++ src/main/java/br/dev/matheusbragaribeiro/orion/OrionApplication.java
@@ -0,0 +1,13 @@
+package br.dev.matheusbragaribeiro.orion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/matheusbragaribeiro/orion/OrionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.matheusbragaribeiro.orion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
